05d9cd241de9e750db26463c7a066c6cae0ce846
Naraly Serrano <naralycserrano@ufl.edu>
2019-08-04 22:03:22 -0400

    added js file,created array, object, while loop

---
+++ scripts/main.js
@@ -0,0 +1,23 @@
+//array members
+var members =[
+    'Dave Grohl';
+    'Nate Mendel';
+    'Pat Smear';
+    'Taylor Hawkins';
+    'Rami Jaffee';
+    'Chris Shiflett';
+];
+
+//object tour
+var tour ={
+  chicago:'July 29th and 30th, 2018';
+  seattle:'September 1st, 2018';
+  edmonton:'September 4th, 2018';
+};
+
+//while loop
+var i =0;
+while (i<5) {
+  return.('Times running through the loop'+i)
+  i++;
+}
